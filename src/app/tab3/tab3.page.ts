52bba94141dfe4001a26fc166a8bb66480c7a74f
Junhan <junhan@qiren.com.sg>
2022-04-27 18:00:46 +0800

    feat: +swiper but styles are not ready

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -5,8 +5,39 @@ import { Component } from '@angular/core';
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
+
 export class Tab3Page {
+
+  headers: ShortcutButton[]= [
+    {
+      logo: 'wallet',
+      name: 'Top Up'
+    },
+    {
+      logo: 'scan-circle',
+      name: 'Scan to Pay'
+    },
+    {
+      logo: 'trending-up',
+      name: 'Transfer'
+    },
+    {
+      logo: 'arrow-down-sharp',
+      name: 'Receive'
+    },
+    {
+      logo: 'globe',
+      name: 'Remittance'
+    },
+  ];
 
   constructor() {}
 
+
+
+}
+
+interface ShortcutButton {
+  logo: string;
+  name: string;
 }
